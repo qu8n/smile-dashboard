ddc186ccec815e021c45364387590cbb7fd3b0b4
ao508 <15623749+ao508@users.noreply.github.com>
2022-07-01 13:28:53 -0400

    Refactoring to match cbioportal-frontend file structure (#3)
    - Expanded documentation in README
    - Refactoring to match the [cbioportal-frontend](https://github.com/cBioPortal/cbioportal-frontend) file structure
    - Set explicit versions of node/yarn for running the dashboard
    
    Signed-off-by: Angelica Ochoa <15623749+ao508@users.noreply.github.com>
    
    Signed-off-by: Angelica Ochoa <15623749+ao508@users.noreply.github.com>

---
+++ src/pages/app/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+// https://reactjs.org/
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          SMILE Dashboard Home
+        </h1>
+        <p>
+          Hello world!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
