48789009a8d01264a22ba7de83987301d5edfc49
ao508 <15623749+ao508@users.noreply.github.com>
2022-07-06 15:20:29 -0400

    Migrate to Apollo client, typescript. Remove mobx (#7)
    Signed-off-by: Angelica Ochoa <15623749+ao508@users.noreply.github.com>

--- src/pages/app/App.tsx
+++ src/pages/app/App.tsx
@@ -1,12 +1,12 @@
+import React from "react";
 import "./App.css";
-// https://reactjs.org/
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>SMILE Dashboard Home</h1>
-        <p>Hello world!</p>
+        <p>Hello, world!</p>
       </header>
     </div>
   );
