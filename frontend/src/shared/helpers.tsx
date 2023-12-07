38018d2cfba7609ae5f81b98e1508b2f9b29d7e2
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-12-07 15:07:20 -0500

    Add 12-245 parts A and C consent status fields to the Patients table (#104)
    * Modify the PatientsList GraphQL query to return additionalProperties field
    
    * Render Consent Status fields from additionalProperties on Patients table
    
    * Rename consent status field names to include '12-245'

--- frontend/src/shared/helpers.tsx
+++ frontend/src/shared/helpers.tsx
@@ -199,6 +199,26 @@ export const PatientsListColumns: ColDef[] = [
     sortable: false,
   },
   {
+    headerName: "12-245 Part A",
+    valueGetter: function ({ data }) {
+      return JSON.parse(
+        data["isAliasPatients"][0].hasSampleSamples[0]
+          ?.hasMetadataSampleMetadata[0]?.additionalProperties
+      )["consent-parta"];
+    },
+    sortable: false,
+  },
+  {
+    headerName: "12-245 Part C",
+    valueGetter: function ({ data }) {
+      return JSON.parse(
+        data["isAliasPatients"][0].hasSampleSamples[0]
+          ?.hasMetadataSampleMetadata[0]?.additionalProperties
+      )["consent-partc"];
+    },
+    sortable: false,
+  },
+  {
     field: "smilePatientId",
     headerName: "SMILE Patient ID",
     valueGetter: function ({ data }) {
