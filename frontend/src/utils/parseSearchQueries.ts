abd55ae98fa663bb1128138bc6e9f53e3af5d246
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2024-08-08 13:14:19 -0400

    Refactor search str parsing function to consider quoted str as a single term

--- frontend/src/utils/parseSearchQueries.ts
+++ frontend/src/utils/parseSearchQueries.ts
@@ -1,19 +1,30 @@
 import { chain } from "lodash";
 
 export function parseUserSearchVal(userSearchVal: string): string[] {
-  return chain(userSearchVal)
-    .split(/[\s,]+/) // split on whitespaces and commas
-    .compact()
-    .uniq()
-    .map((val) => {
-      val = val.trim();
-      // Add back leading 0s to queries that were copied from Excel, where leading 0s
-      // of number cells are removed by default (e.g. `01234` becomes `1234`).
-      const isNumber = /^\d*$/.test(val);
-      if (isNumber) {
-        return val.padStart(5, "0");
-      }
-      return val;
-    })
-    .value();
+  return (
+    chain(userSearchVal)
+      // Split on space and comma delimiters, but ignore them inside single/double quotes. Breakdown:
+      // [\s,]+ matches >= 1 whitespace/comma characters
+      // (?=...) is a positive lookahead, "match the previous pattern only if it's followed by ..."
+      // (?:...) is a non-capturing group that groups the pattern inside it without capturing it
+      // [^'"] matches any non-quote character
+      // '[^']*' and "[^"]*" match single and double quoted strings, respectively
+      // *$ asserts that the lookahead pattern occurs >= 0 times until the end of the string
+      .split(/[\s,]+(?=(?:[^'"]|'[^']*'|"[^"]*")*$)/)
+      .compact()
+      .uniq()
+      .map((val) => {
+        val = val.trim();
+        // Add back leading 0s to queries that were copied from Excel, where leading 0s
+        // of number cells are removed by default (e.g. `01234` becomes `1234`).
+        const isNumber = /^\d*$/.test(val);
+        if (isNumber) {
+          return val.padStart(5, "0");
+        }
+        // Remove single/double quotes from quoted strings
+        val = val.replace(/^['"]|['"]$/g, "");
+        return val;
+      })
+      .value()
+  );
 }
