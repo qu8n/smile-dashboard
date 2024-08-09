168f888c3ecae25dd284bddecf73d9c75ab61c5f
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2024-08-09 14:36:20 -0400

    Handle a single search value for cancerType/Detailed search

--- graphql-server/src/schemas/neo4j.ts
+++ graphql-server/src/schemas/neo4j.ts
@@ -30,6 +30,8 @@ import {
   generateFieldResolvers,
   sortArrayByNestedField,
 } from "../utils/flattening";
+import { ApolloServerContext } from "../utils/servers";
+import { CachedOncotreeData } from "../utils/oncotree";
 
 type SortOptions = { [key: string]: SortDirection }[];
 
@@ -336,9 +338,50 @@ function buildResolvers(
         }
         return patients.slice(args.options.offset, args.options.limit + 1);
       },
-      async samples(_source: undefined, args: any) {
+      async samples(
+        _source: undefined,
+        args: any,
+        { oncotreeCache }: ApolloServerContext
+      ) {
+        let customWhere = args.where;
+
+        const sampleMetadataFilters = (customWhere?.OR as SampleWhere[])?.find(
+          (filter) => filter.hasMetadataSampleMetadata_SOME?.OR
+        )?.hasMetadataSampleMetadata_SOME?.OR;
+
+        if (sampleMetadataFilters && sampleMetadataFilters?.length > 0) {
+          const searchInput = Object.values(sampleMetadataFilters[0])[0];
+
+          const oncotreeCodes = oncotreeCache.keys();
+          for (const code of oncotreeCodes) {
+            const oncotreeData = oncotreeCache.get(code) as CachedOncotreeData;
+            const { name, mainType } = oncotreeData!;
+
+            if (typeof searchInput === "string") {
+              if (name?.toLowerCase().includes(searchInput.toLowerCase())) {
+                customWhere.OR.find(
+                  (filter: SampleWhere) =>
+                    filter.hasMetadataSampleMetadata_SOME?.OR
+                ).hasMetadataSampleMetadata_SOME.OR.push({
+                  oncotreeCode_IN: code,
+                });
+                break;
+              }
+              if (mainType?.toLowerCase().includes(searchInput.toLowerCase())) {
+                customWhere.OR.find(
+                  (filter: SampleWhere) =>
+                    filter.hasMetadataSampleMetadata_SOME?.OR
+                ).hasMetadataSampleMetadata_SOME.OR.push({
+                  oncotreeCode_IN: code,
+                });
+                break;
+              }
+            }
+          }
+        }
+
         const samples = await ogm.model("Sample").find({
-          where: args.where,
+          where: customWhere,
           options: args.options,
           selectionSet: `{
             datasource
