463670ff8a14e82cb0cbe7389ae9a5776a6cbd0a
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2025-08-04 18:44:48 -0400

    Create a standalone component for AG Grid table
    Signed-off-by: Quan Nguyen <86090707+qu8n@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,10 @@
-import { useEffect, useState } from "react";
+import {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
 import { Routes, Route } from "react-router-dom";
 import RequestsPage from "./pages/requests/RequestsPage";
 import PatientsPage from "./pages/patients/PatientsPage";
@@ -7,6 +13,17 @@ import CohortsPage from "./pages/cohorts/CohortsPage";
 import LoginSuccessPage from "./pages/auth/LoginSuccessPage";
 import SmileNavBar from "./shared/components/SmileNavBar";
 import { getUserEmail } from "./utils/getUserEmail";
+
+// TODO: create a provider component and move the state logic there
+// and wrap the <UserEmailContext.Provider> around <App> in index.tsx
+// TODO: stop passing userEmail and setUserEmail as props to components
+export const UserEmailContext = createContext<{
+  userEmail: string | null;
+  setUserEmail: Dispatch<SetStateAction<string | null>>;
+}>({
+  userEmail: null,
+  setUserEmail: () => null,
+});
 
 export default function App() {
   const [userEmail, setUserEmail] = useState<string | null>(null);
@@ -21,50 +38,43 @@ export default function App() {
 
   return (
     <main id="main" className="main">
-      <SmileNavBar userEmail={userEmail} setUserEmail={setUserEmail} />
-      <Routes>
-        <>
-          <Route
-            path="/"
-            element={
-              <RequestsPage userEmail={userEmail} setUserEmail={setUserEmail} />
-            }
-          >
-            <Route path=":igoRequestId" />
-          </Route>
-          <Route
-            path="/requests/"
-            element={
-              <RequestsPage userEmail={userEmail} setUserEmail={setUserEmail} />
-            }
-          >
-            <Route path=":igoRequestId" />
-          </Route>
-          <Route
-            path="/patients/"
-            element={
-              <PatientsPage userEmail={userEmail} setUserEmail={setUserEmail} />
-            }
-          >
-            <Route path=":patientId" />
-          </Route>
-          <Route
-            path="/samples"
-            element={
-              <SamplesPage userEmail={userEmail} setUserEmail={setUserEmail} />
-            }
-          />
-          <Route
-            path="/cohorts/"
-            element={
-              <CohortsPage userEmail={userEmail} setUserEmail={setUserEmail} />
-            }
-          >
-            <Route path=":cohortId" />
-          </Route>
-          <Route path="/auth/login-success" element={<LoginSuccessPage />} />
-        </>
-      </Routes>
+      <UserEmailContext.Provider value={{ userEmail, setUserEmail }}>
+        <SmileNavBar userEmail={userEmail} setUserEmail={setUserEmail} />
+        <Routes>
+          <>
+            <Route path="/" element={<RequestsPage />}>
+              <Route path=":igoRequestId" />
+            </Route>
+            <Route path="/requests/" element={<RequestsPage />}>
+              <Route path=":igoRequestId" />
+            </Route>
+            <Route
+              path="/patients/"
+              element={
+                <PatientsPage
+                  userEmail={userEmail}
+                  setUserEmail={setUserEmail}
+                />
+              }
+            >
+              <Route path=":patientId" />
+            </Route>
+            <Route path="/samples" element={<SamplesPage />} />
+            <Route
+              path="/cohorts/"
+              element={
+                <CohortsPage
+                  userEmail={userEmail}
+                  setUserEmail={setUserEmail}
+                />
+              }
+            >
+              <Route path=":cohortId" />
+            </Route>
+            <Route path="/auth/login-success" element={<LoginSuccessPage />} />
+          </>
+        </Routes>
+      </UserEmailContext.Provider>
     </main>
   );
 }
---
+++ frontend/src/components/DataTable.tsx
@@ -0,0 +1,194 @@
+import { AgGridReact } from "ag-grid-react";
+import AutoSizer from "react-virtualized-auto-sizer";
+import styles from "./records.module.scss";
+import { handleAgGridPaste } from "../utils/handleAgGridPaste";
+import {
+  RefObject,
+  ClipboardEvent,
+  useState,
+  useContext,
+  Dispatch,
+  SetStateAction,
+} from "react";
+import { CellEditRequestEvent, ColDef } from "ag-grid-community";
+import {
+  CACHE_BLOCK_SIZE,
+  defaultColDef,
+  isValidCostCenter,
+  SampleChange,
+} from "../shared/helpers";
+import { UserEmailContext } from "../App";
+import { getUserEmail } from "../utils/getUserEmail";
+import { openLoginPopup } from "../utils/openLoginPopup";
+
+const COST_CENTER_VALIDATION_ALERT =
+  "Please update your Cost Center/Fund Number input as #####/##### " +
+  "(5 digits, a forward slash, then 5 digits). For example: 12345/12345.";
+
+interface DataTableProps {
+  gridRef: RefObject<AgGridReact>;
+  hasParams?: boolean;
+  setAlertContent: Dispatch<SetStateAction<string | null>>;
+  columnDefs: ColDef[];
+}
+
+export function DataTable({
+  gridRef,
+  hasParams = false,
+  setAlertContent,
+  columnDefs,
+}: DataTableProps) {
+  const { userEmail, setUserEmail } = useContext(UserEmailContext);
+  const [changes, setChanges] = useState<SampleChange[]>([]);
+
+  async function handleCellEditRequest(params: CellEditRequestEvent) {
+    const primaryId = params.data.primaryId;
+    const fieldName = params.colDef.field!;
+    const { oldValue, newValue, node: rowNode } = params;
+
+    // Prevent registering a change if no actual changes are made
+    const noChangeInVal = rowNode.data[fieldName] === newValue;
+    const noChangeInEmptyCell = !rowNode.data[fieldName] && !newValue;
+    if (noChangeInVal || noChangeInEmptyCell) {
+      setChanges((prevChanges) => {
+        const updatedChanges = prevChanges.filter(
+          (c) => !(c.primaryId === primaryId && c.fieldName === fieldName)
+        );
+        // TODO: test that this is ok
+        // if (updatedChanges.length === 0) setUnsavedChanges?.(false);
+        return updatedChanges;
+      });
+      gridRef.current?.api?.redrawRows({ rowNodes: [rowNode] });
+      return;
+    }
+
+    // Add/update the billedBy cell to/in the changes array
+    if (fieldName === "billed" && setUserEmail) {
+      let currUserEmail = userEmail;
+
+      if (!currUserEmail) {
+        currUserEmail = await new Promise<string | null>((resolve) => {
+          window.addEventListener("message", handleLogin);
+
+          function handleLogin(event: MessageEvent) {
+            if (event.data === "success") {
+              getUserEmail().then((email) => {
+                window.removeEventListener("message", handleLogin);
+                resolve(email);
+              });
+            }
+          }
+
+          openLoginPopup();
+        });
+
+        if (!currUserEmail) return;
+        setUserEmail(currUserEmail);
+      }
+
+      const currUsername = currUserEmail.split("@")[0];
+
+      setChanges((changes) => {
+        const billedBy = changes.find(
+          (c) => c.primaryId === primaryId && c.fieldName === "billedBy"
+        );
+        if (billedBy) {
+          billedBy.newValue = currUsername;
+        } else {
+          changes.push({
+            primaryId,
+            fieldName: "billedBy",
+            oldValue: "",
+            newValue: currUsername,
+            rowNode,
+          });
+        }
+        return [...changes];
+      });
+    }
+
+    // Add/update the edited cell to/in the changes array
+    setChanges((changes) => {
+      const change = changes.find(
+        (c) => c.primaryId === primaryId && c.fieldName === fieldName
+      );
+      if (change) {
+        change.newValue = newValue;
+      } else {
+        changes.push({ primaryId, fieldName, oldValue, newValue, rowNode });
+      }
+      return [...changes];
+    });
+
+    // Validate Cost Center inputs
+    if (fieldName === "costCenter" && !isValidCostCenter(newValue)) {
+      setAlertContent(COST_CENTER_VALIDATION_ALERT);
+    }
+
+    // TODO: wherever setUnsavedChanges is used, change the logic to check changes array length
+    // setUnsavedChanges?.(true);
+    gridRef.current?.api?.redrawRows({ rowNodes: [rowNode] });
+  }
+
+  async function handlePaste(e: ClipboardEvent<HTMLDivElement>) {
+    try {
+      await handleAgGridPaste({ e, gridRef, handleCellEditRequest });
+    } catch (error) {
+      if (error instanceof Error) {
+        setAlertContent(error.message);
+      } else {
+        console.error("Unexpected error during paste:", error);
+      }
+    }
+  }
+
+  return (
+    <AutoSizer>
+      {({ width }) => {
+        return (
+          <div
+            className={`ag-theme-alpine ${
+              hasParams ? styles.popupTableHeight : styles.tableHeight
+            }`}
+            style={{ width }}
+            onPaste={handlePaste}
+          >
+            <AgGridReact
+              ref={gridRef}
+              rowModelType="serverSide"
+              serverSideInfiniteScroll={true}
+              cacheBlockSize={CACHE_BLOCK_SIZE}
+              columnDefs={columnDefs}
+              defaultColDef={defaultColDef}
+              enableRangeSelection={true}
+              onGridReady={(params) => params.api.sizeColumnsToFit()}
+              onFirstDataRendered={(params) =>
+                params.columnApi.autoSizeAllColumns()
+              }
+              // TODO: test that this is ok. Samples tabs change cols but we fetch all data at once
+              // Why should we refresh data on column change?
+              // onGridColumnsChanged={() => refreshData(userSearchVal)}
+              context={{
+                getChanges: () => changes,
+              }}
+              rowClassRules={{
+                unlocked: function (params) {
+                  return params.data?.revisable === true;
+                },
+                locked: function (params) {
+                  return params.data?.revisable === false;
+                },
+              }}
+              onCellEditRequest={handleCellEditRequest}
+              readOnlyEdit={true}
+              tooltipShowDelay={0}
+              tooltipHideDelay={60000}
+              tooltipMouseTrack={true}
+              suppressClipboardPaste={true}
+            />
+          </div>
+        );
+      }}
+    </AutoSizer>
+  );
+}
